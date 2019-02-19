722776317e3373ed8538f107f23184ed72d7aacf
Tolga YILDIZ <yildiztolgaa@gmail.com>
2019-02-19 09:50:29 +0300

    Movies Router add

---
+++ helper/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb+srv://movie_user:cml180183@movie-api-jmxml.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
+    mongoose.connection.on('open', ()=> {
+        console.log('MongoDB: Connected');
+    });
+    mongoose.connection.on('error', (err)=> {
+        console.log('MongoDB: Has a error',err);
+    });
+    mongoose.Promise = global.Promise;
+    
+}
